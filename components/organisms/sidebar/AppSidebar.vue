b47d73b048072dadb47fd96ac89bb445d5413ea2
JeongHwanRoh <rohjh951@naver.com>
2026-01-08 16:39:55 +0900

    fix: AppSidebar 내 일부 ui 개선

--- components/organisms/sidebar/AppSidebar.vue
+++ components/organisms/sidebar/AppSidebar.vue
@@ -1,5 +1,8 @@
 <template>
   <aside class="sidebar">
+    <div class="userZone">
+      {{name}}님, 환영합니다!
+    </div>
     <!-- 사이드바 메뉴 -->
     <sideBarMenu :menu="menu" :active-path="route.path" @navigate="go" />
     <!-- 로그아웃 버튼 -->
@@ -11,9 +14,13 @@
 import { useRouter, useRoute } from '#app'
 import sideBarMenu from '~/components/molecules/sidebar/sideBarMenu.vue'
 import sideBarLogout from '~/components/molecules/sidebar/sideBarLogout.vue'
+import { useUserStore } from '~/stores/userStore'
 
 const router = useRouter()
 const route = useRoute()
+const userStore = useUserStore();
+const name=userStore.name;
+console.log("사이드바에서 불러온 유저 이름:",name);
 
 const menu = [
   { label: '메인대시보드', path: '/dashboard' },
@@ -34,5 +41,15 @@ const go = (path: string) => {
   background-color: #1b2f63;
   color: white;
   padding: 1rem;
+
+  display: flex;
+  flex-direction: column;
+}
+
+.userZone {
+  width: 90%;              /* sidebar 기준 90% */
+  white-space: nowrap;     /* 줄바꿈 방지 */
+  margin-bottom: 1rem;
+  font-weight: 500;
 }
 </style>
